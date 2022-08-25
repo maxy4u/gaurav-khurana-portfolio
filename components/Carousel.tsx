d23f4193d481414f4529fe249db4b1018cbb4522
Gaurav Khurana <gauravpro2019@Gauravs-MacBook-Pro.local>
2022-08-25 14:12:29 -0300

    fix: lihgt theme background image

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -88,8 +88,11 @@ function Card({ src, index, current, handleCardClick, headline }: TCardComp): JS
 //-----------------------------
 
 const CardControl = ({ type, title, handleClick }: TCardControl) => {
+  const {
+    state: { theme }
+  } = useAppContext();
   return (
-    <button className={`btn btn--${type}`} title={title} onClick={handleClick}>
+    <button className={`${theme} btn btn--${type}`} title={title} onClick={handleClick}>
       <svg className='icon' viewBox='0 0 24 24'>
         <path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' />
       </svg>
