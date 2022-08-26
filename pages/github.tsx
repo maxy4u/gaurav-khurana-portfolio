e70b486d197f90ee94a9688b40e4e2539a68bab7
Gaurav Khurana <gauravpro2019@Gauravs-MacBook-Pro.local>
2022-08-26 19:58:27 -0300

    fix: Build error for graphql on github page

--- pages/github.tsx
+++ pages/github.tsx
@@ -1,32 +1,54 @@
 import { memo, FC, useState, useCallback, useMemo } from 'react';
-import client from '../client/apollo-client';
-import { ApolloQueryResult, gql } from '@apollo/client';
+// import client from '../client/apollo-client';
+import { gql, useQuery } from '@apollo/client';
 import { Layout, SearchBox } from '../components';
 import stylesHome from '../styles/Home.module.css';
 import { TRepositories } from './api/resolvers';
 import styles from '../styles/GitHub.module.css';
 
-interface TRepos {
-  repositories: Array<TRepositories>;
-}
-
 interface TRepoData {
   data: {
-    getRepos: TRepositories[];
+    getRepos: TRepositories[] | [];
   };
 }
 
-interface TGitHub extends TRepos {
+interface TGitHub {
   user: string;
 }
 
-const GitHub: FC<TGitHub> = ({ repositories, user }) => {
+const Query = gql`
+  query GetRepositories {
+    getRepos {
+      id
+      name
+      owner {
+        avatar_url
+        login
+      }
+      url
+      description
+    }
+  }
+`;
+
+const GitHub: FC<TGitHub> = ({ user }) => {
   const [query, setQuery] = useState<string | null>(null);
+  const { data, loading, error } = useQuery<TRepoData['data']>(Query);
+  const { getRepos: repositories }: { getRepos: TRepositories[] | [] } = data || { getRepos: [] };
+
   const onSearch = useCallback((value: string) => {
     setQuery(value);
   }, []);
   const regexp = useMemo(() => new RegExp(`${query}`, 'gi'), [query]);
-  const serchResult = query && repositories.filter(({ name }) => regexp.test(name));
+  const serchResult = query && repositories.filter(({ name }: { name: string }) => regexp.test(name));
+
+  if (loading) {
+    return <h2>Loading Data...</h2>;
+  }
+
+  if (error) {
+    return <h2>Sorry, there&aposs been an error...</h2>;
+  }
 
   return (
     <Layout {...{ user }}>
@@ -34,7 +56,7 @@ const GitHub: FC<TGitHub> = ({ repositories, user }) => {
         <SearchBox onSearch={onSearch} placeholder='Search repos ...' />
         <h2 className={styles.h2}>List of Repositories</h2>
         <ul className={styles.repos}>
-          {(serchResult || repositories).map(({ id, description, name, url }) => (
+          {(serchResult || repositories).map(({ id, description, name, url }: Partial<TRepositories>) => (
             <li key={id} className={styles.list}>
               <a href={url} title={description} target='_blank' rel='noreferrer'>
                 {name}
@@ -48,27 +70,34 @@ const GitHub: FC<TGitHub> = ({ repositories, user }) => {
 };
 
 export async function getServerSideProps() {
-  const {
-    data: { getRepos: repositories }
-  }: ApolloQueryResult<TRepoData['data']> = await client.query({
-    query: gql`
-      query GetRepositories {
-        getRepos {
-          id
-          name
-          owner {
-            avatar_url
-            login
-          }
-          url
-          description
-        }
-      }
-    `
-  });
+  //
+  /* 
+  keeping below comment for my reference 
+  Below logic will fail the build as we are pointing  to same app's "api/graphql"
+  Since getStaticProps and getServerSide props gets executed at build time the query 
+  will fail because "api/graphql" is not ready as it is internal url
+  So while executing api call in these two function make sure the url is external
+  */
+  // const {
+  //   data: { getRepos: repositories }
+  // }: ApolloQueryResult<TRepoData['data']> = await client.query({
+  //   query: gql`
+  //     query GetRepositories {
+  //       getRepos {
+  //         id
+  //         name
+  //         owner {
+  //           avatar_url
+  //           login
+  //         }
+  //         url
+  //         description
+  //       }
+  //     }
+  //   `
+  // });
   return {
     props: {
-      repositories: repositories,
       user: process.env.user
     }
   };
