1ce88c120458e36a0c107db5d3ff6cad79934d84
Gaurav Khurana <gauravpro2019@Gauravs-MacBook-Pro.local>
2022-08-31 22:10:06 -0300

    chore: Website Thumbnail meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta charSet='UTF-8' />
         <meta property='og:title' content='Gaurav Khurana Portfolio' />
-        <meta property='og:image' content='/images/gauravkhurana-thumbnail' />
+        <meta property='og:image' content='/images/gauravkhurana-thumbnail.jpg' />
         <meta property='og:description' content='Gaurav Khurana Ui Developer UI Architect portfolio nextjs app' />
         <meta property='og:url' content='https://gauravkhurana.ca' />
         <meta property='og:image:width' content='1125' />
